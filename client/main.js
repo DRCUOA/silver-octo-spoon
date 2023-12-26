19098bc827690145cc264c1e86bf9ada85d738d9
Richard Clark <dcla839@auckland.uni.ac.nz>
2023-12-26 15:44:03 +1300

    l5

--- client/main.js
+++ client/main.js
@@ -1,9 +1,17 @@
 const app =  Vue.createApp({
   data() {
     return {
-      title: "The Final Empire",
-      tickets: 10,
-      image: "./assets/sample-img.jpg",
+      title: "Show Events",
+      tickets: 50,
+      image: "./assets/sample-img.jpeg",
+      details: ["Comedy", "Musical", "Satire", "GameShow", "Book Reading"],
+      cart: 0
+    }
+  },
+  methods: {
+    updateCart() {
+      cart++;
+      tickets--;
     }
   }
 });
